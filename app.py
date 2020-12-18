c882fe9ad2977603f2461eb67613d49ad2d0ec1c
Raja Raman <raja.raman@kwikee.com>
2020-12-18 15:14:26 -0500

    Updates for 2020-12-18

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from flask import Flask, request, jsonify
-from flask_restful import Resource, Api
-from sqlalchemy import create_engine
+from flask import Flask, request, jsonify, render_template
 from json import dumps
 import os
 
@@ -9,47 +7,30 @@ import string_util as su
 
 
 app = Flask(__name__)
-api = Api(app)
-
 
 '''
     http://localhost:8071/
 '''
 @app.route("/")
 def main():
-	return "Welcome"
-
-
-'''
-    http://localhost:8071/env
-'''
-@app.route("/env")
-def show_env():
-
-    env = int(os.environ.get('WORKER_SMTP_PORT', '28'))
 
     result = {
-        'env' : env
+        'result' : "1.7"
     }
 
-    return jsonify(result) 
+    app.logger.info('Inside api')
+
+    return render_template('main.html')
+
 
 '''
-    http://localhost:8071/reverse
-
-    http://localhost:8071/reverse?content=hello
+    http://localhost:8071/api
 '''
-@app.route("/reverse")
-def reverse_string_api():
-
-    content = request.values.get('content')
-
-    print('c : ', content)
-
-    r_content = su.reverse_string(content)
+@app.route("/api")
+def api():
 
     result = {
-        'content' : r_content
+        'content' : 'sdf'
     }
 
     return jsonify(result) 
